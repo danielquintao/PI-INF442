bbdb07918f41bb38d1ac2380329b85c904a63dfa
daniel <danielquintaodemoraes@gmail.com>
2020-05-09 23:04:04 +0200

    changed srand seed

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@ void ER(graph& g,double prob){
     double s=0;
     for(std::vector<point> ::iterator p=g.VertexList.begin();p!=g.VertexList.end();p++){
         for(std::vector<point> ::iterator q=g.VertexList.begin();q!=p&&q!=g.VertexList.end();q++){
-            unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-		    srand(seed);
             s=rand()/RAND_MAX;
             if(s<=prob){
                 g.AddEdge(*p,*q);
@@ -21,6 +19,9 @@ void ER(graph& g,double prob){
 
 int main(int argc, char const *argv[])
 {
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    srand(seed);
+    
     graph g;
 
     return 0;
